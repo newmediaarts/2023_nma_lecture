22b448ea9a08ecc9b27ec7079e6149f6d31be419
David Efhan <davidefhan@Davids-MacBook-Pro.local>
2023-02-16 02:00:45 -1000

    feat: add vite config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve, join } from "path";
+import { defineConfig } from "vite";
+import glob from "glob";
+
+export default defineConfig({
+  root: join(__dirname, "src"),
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: join(__dirname, "dist"),
+    rollupOptions: {
+      input: glob.sync(resolve(__dirname, "src", "**/*.html")),
+    },
+  },
+});
