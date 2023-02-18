694191ad5bd7020aca90a97d707eb93cd1ba2f22
David Efhan <davidefhan@Davids-MacBook-Pro.local>
2023-02-17 23:21:43 -1000

    feat: update base and rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import glob from "glob";
 
 export default defineConfig({
-  base: "./dist",
+  base: "./",
   root: join(__dirname, "src"),
   server: {
     port: 3000,
